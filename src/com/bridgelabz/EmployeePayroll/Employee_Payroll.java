19ccc09aff50eef4cf60ccd8e630dbb295209a40
champ09D <darshanmhatre709@gmail.com>
2022-08-16 15:18:10 +0530

    Added : Refactor Retrieve Data Case Add

--- src/com/bridgelabz/EmployeePayroll/Employee_Payroll.java
+++ src/com/bridgelabz/EmployeePayroll/Employee_Payroll.java
@@ -1,45 +1,31 @@
 package com.bridgelabz.EmployeePayroll;
-
-import java.io.DataInputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Employee_Payroll
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         try {
-            System.out.println("----------SQL Operations------------");
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            DataInputStream d = new DataInputStream(System.in);
-            String dbURL = "jdbc:mysql://localhost/payroll_service";
-            String username = "root";
+            String JdbcURL = "jdbc:mysql://localhost/payroll_service";
+            String Username = "root";
             String password = "yuktadarsh";
-            Connection conn = DriverManager.getConnection(dbURL, username, password);
-            Statement stmt = conn.createStatement();
-            System.out.println("Connection successful :-" + conn);
-
-            System.out.println("Enter You Want Update Salary ");
-            double salary = Integer.parseInt( d.readLine());
-
-            System.out.println("Enter You Want that update Name    ");
-            String up_name = d.readLine();
-
-            PreparedStatement ps = conn.prepareStatement(
-                    "UPDATE employee_payroll SET salary = ? WHERE name = ?");
-
-            ps.setDouble(1, salary);
-            ps.setString(2, up_name);
-            ps.executeUpdate();
-            System.out.println(salary+"Updated Succesfully");
-            ps.close();
-
-
-        }catch (Exception e)
+            Connection con = null;
+            PreparedStatement pstmt = null;
+            ResultSet rst = null;
+            String myQuery = "select * from employee_payroll";
+            con = DriverManager.getConnection(JdbcURL, Username, password);
+            System.out.println("Conne");
+            pstmt = con.prepareStatement(myQuery);
+            rst = pstmt.executeQuery();
+            System.out.println("Id\t\tName\t\tAge\t\tState");
+            while (rst.next()) {
+                System.out.print(rst.getString(1));
+                System.out.print("\t\t" + rst.getString(2));
+                System.out.print("\t\t" + rst.getString(3));
+                System.out.println("\t\t" + rst.getString(4));
+            }
+        } catch (Exception e)
         {
-            System.out.println(e.getMessage());
+            System.out.println(e);
         }
     }
 }
