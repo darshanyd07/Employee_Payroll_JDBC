b3b20cd2445d339dca140c22851b20589fae99df
champ09D <darshanmhatre709@gmail.com>
2022-08-16 10:38:11 +0530

    Added : Retrieve The Employee Payroll Data  Case File Add

--- src/com/bridgelabz/EmployeePayroll/Employee_Payroll.java
+++ src/com/bridgelabz/EmployeePayroll/Employee_Payroll.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.EmployeePayroll;
+import java.io.DataInputStream;
 import java.sql.*;
 
 public class Employee_Payroll
@@ -9,11 +10,34 @@ public class Employee_Payroll
         {
             System.out.println("----------SQL Operations------------");
             Class.forName("com.mysql.cj.jdbc.Driver");
+            DataInputStream d = new DataInputStream(System.in);
             String dbURL = "jdbc:mysql://localhost/payroll_service";
             String username = "root";
             String password = "yuktadarsh";
             Connection conn = DriverManager.getConnection(dbURL,username,password);
+            Statement stmt = conn.createStatement();
             System.out.println("Connection successful :-"+conn);
+
+            System.out.println("Enter Table Name ");
+            String table = d.readLine();
+            String sql = "select * from "+table+";";
+            ResultSet rs = stmt.executeQuery(sql);
+            System.out.println("Id   E_name   E_age   E_Salary    E_city");
+            System.out.println("-----------------------------");
+            while (rs.next())
+            {
+                int eid = rs.getInt("Id");
+                String ename = rs.getString("Name");
+                int eage = rs.getInt("Age");
+                Double esalary = rs.getDouble("Salary");
+                String ecity = rs.getString("City");
+
+                System.out.println(+eid+"   "+ename+"   "+eage+"   " +esalary+  "    " +ecity   );
+            }
+            System.out.println("-----------------------------");
+            System.out.println("Display Table  Successfully");
+            System.out.println("-----------------------------");
+            System.exit(0);
         }
         catch (Exception e)
         {
