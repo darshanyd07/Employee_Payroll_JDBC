54d74b13866158de3c93818a8e3af0ce8cbb4c3d
champ09D <darshanmhatre709@gmail.com>
2022-08-17 14:51:52 +0530

    Added : Retrieve Data Joing Date Between Case File Add

--- src/com/bridgelabz/EmployeePayroll/Employee_Payroll.java
+++ src/com/bridgelabz/EmployeePayroll/Employee_Payroll.java
@@ -11,17 +11,19 @@ public class Employee_Payroll
             Connection con = null;
             PreparedStatement pstmt = null;
             ResultSet rst = null;
-            String myQuery = "select * from employee_payroll";
+            String myQuery = "select * from employee_payroll where Join_Date BETWEEN  '1999-01-14' AND  '1999-05-14';";
             con = DriverManager.getConnection(JdbcURL, Username, password);
             System.out.println("Conne");
             pstmt = con.prepareStatement(myQuery);
             rst = pstmt.executeQuery();
-            System.out.println("Id\t\tName\t\tAge\t\tState");
+            System.out.println("Id\t\tName\t\tAge\t\tState\t\tJoin Date");
             while (rst.next()) {
                 System.out.print(rst.getString(1));
                 System.out.print("\t\t" + rst.getString(2));
                 System.out.print("\t\t" + rst.getString(3));
                 System.out.println("\t\t" + rst.getString(4));
+                System.out.println("  " + rst.getString(15));
+
             }
         } catch (Exception e)
         {
