932079a39f775b40a6aa1b12c50a5512a9e73868
champ09D <darshanmhatre709@gmail.com>
2022-08-17 16:17:09 +0530

    Added : Aggregate Function Case File Add

--- src/com/bridgelabz/EmployeePayroll/Employee_Payroll.java
+++ src/com/bridgelabz/EmployeePayroll/Employee_Payroll.java
@@ -3,7 +3,8 @@ import java.sql.*;
 
 public class Employee_Payroll
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         try {
             String JdbcURL = "jdbc:mysql://localhost/payroll_service";
             String Username = "root";
@@ -11,13 +12,14 @@ public class Employee_Payroll
             Connection con = null;
             PreparedStatement pstmt = null;
             ResultSet rst = null;
-            String myQuery = "select * from employee_payroll where Join_Date BETWEEN  '1999-01-14' AND  '1999-05-14';";
+            String myQuery = "select  SUM(salary), AVG(salary), MAX(salary), MIN(salary)   from employee_payroll   group by Gender  ;";
             con = DriverManager.getConnection(JdbcURL, Username, password);
             System.out.println("Conne");
             pstmt = con.prepareStatement(myQuery);
             rst = pstmt.executeQuery();
-            System.out.println("Id\t\tName\t\tAge\t\tState\t\tJoin Date");
-            while (rst.next()) {
+            System.out.println("Sum\t\tAvg\t\tMax\t\tMin");
+            while (rst.next())
+            {
                 System.out.print(rst.getString(1));
                 System.out.print("\t\t" + rst.getString(2));
                 System.out.print("\t\t" + rst.getString(3));
