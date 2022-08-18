25da66abd4eaa5d29d6eb8f9f4d61553a4d9cb24
champ09D <darshanmhatre709@gmail.com>
2022-08-18 15:21:42 +0530

    Added : Cascading Delete Case File Add

--- src/com/bridgelabz/EmployeePayroll/Employee_Payroll.java
+++ src/com/bridgelabz/EmployeePayroll/Employee_Payroll.java
@@ -1,33 +1,84 @@
 package com.bridgelabz.EmployeePayroll;
+import java.io.DataInputStream;
 import java.sql.*;
 
 public class Employee_Payroll
 {
     public static void main(String[] args)
     {
+        DataInputStream  d = new DataInputStream(System.in);
         try {
-            String JdbcURL = "jdbc:mysql://localhost/payroll_service";
-            String Username = "root";
+            System.out.println("----------SQL Operations------------");
+            String dbURL = "jdbc:mysql://localhost/payroll_service";
+            System.out.println("DataBase Has Connected");
+            String username = "root";
             String password = "yuktadarsh";
-            Connection con = null;
-            PreparedStatement pstmt = null;
-            ResultSet rst = null;
-            String myQuery = "select  SUM(salary), AVG(salary), MAX(salary), MIN(salary)   from employee_payroll   group by Gender  ;";
-            con = DriverManager.getConnection(JdbcURL, Username, password);
-            System.out.println("Conne");
-            pstmt = con.prepareStatement(myQuery);
-            rst = pstmt.executeQuery();
-            System.out.println("Sum\t\tAvg\t\tMax\t\tMin");
-            while (rst.next())
+            Connection conn = DriverManager.getConnection(dbURL,username,password);
+            Statement stmt = conn.createStatement();
+            System.out.println("Do You Want To Add New Empolyee Detail ? ");
+            String in_sta = d.readLine();
+            if( in_sta.equals("yes"))
             {
-                System.out.print(rst.getString(1));
-                System.out.print("\t\t" + rst.getString(2));
-                System.out.print("\t\t" + rst.getString(3));
-                System.out.println("\t\t" + rst.getString(4));
-                System.out.println("  " + rst.getString(15));
+                System.out.println("How Many Employees Detilas You Want To Add ? ");
+                int num = Integer.parseInt(d.readLine());
+
+                for( int i = 0 ; i<num;i++)
+                {
+                    System.out.println("Enter Id");
+                    int Id = Integer.parseInt(d.readLine());
+
+                    System.out.println("Enter Employee Name ");
+                    String Name = d.readLine();
+
+                    System.out.println("Enter Address ");
+                    String Address = d.readLine();
+
+                    System.out.println("Enter City ");
+                    String City = d.readLine();
+
+
+                    System.out.println("Enter Zip-Code");
+                    int zip = Integer.parseInt(d.readLine());
+
+                    System.out.println("Enter Phone Number");
+                    int phone = Integer.parseInt(d.readLine());
+
+                    String inserT ="insert into Employees  values("+Id+",'"+Name+"','"+Address+"','"+City+"',"+zip+","+phone+");";
+                    PreparedStatement st = conn.prepareStatement(inserT);
+                    st.execute();
+                    System.out.println("Employee "+Name+ "  Insert Records Successfully");
+                }
 
             }
-        } catch (Exception e)
+            else if(in_sta.equals("no"))
+            {
+                System.out.println("Ok Darsh....");
+            }
+
+            System.out.println("You want Delete Employee Records");
+            String del_sta = d.readLine();
+
+            if( del_sta.equals("yes"))
+            {
+                System.out.println("How Many Employee Records You Want  Delete ? ");
+                int del_num = Integer.parseInt(d.readLine());
+
+                for(int j = 0;j < del_num;j++)
+                {
+                    System.out.println("Enter delete  ID you want to delete ");
+                    int del_id = Integer.parseInt(d.readLine());
+                    String delete = " delete from Employees where Id  = "+del_id+"";
+                    stmt.executeUpdate(delete);
+                    System.out.println("Delete Record Successfully");
+                }
+            }
+            else if(del_sta.equals("no"))
+            {
+                System.out.println("Ok Darsh....");
+            }
+
+        }
+        catch (Exception e)
         {
             System.out.println(e);
         }
